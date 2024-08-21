781ade3b4c61b08d9d140a4f6fcccb0ea0350445
Isutomu <isutomu@gmail.com>
2024-08-21 13:51:27 -0300

    :sparkles: feat: add win/lose management.
    Also allowed the game to properly reset.

--- components/Gameboard.jsx
+++ components/Gameboard.jsx
@@ -1,32 +1,22 @@
 import { useState } from "react";
 import Card from "./Card";
 import { shuffle } from "fast-shuffle";
+import Score from "../components/Score";
 
-export default function Gameboard({ imgUrls, updateScore }) {
+export default function Gameboard({ imgUrls }) {
+  const [bestScore, setBestScore] = useState(0);
+  const [currentScore, setCurrentScore] = useState(0);
   const [cardsIds, setCardsIds] = useState(Object.keys(imgUrls));
   const [cardsSelected, setCardsSelected] = useState([]);
 
-  const score = (() => {
-    const cardsSelectedSet = new Set(cardsSelected);
-    return cardsSelectedSet.size;
-  })();
-  updateScore(score);
-
+  const score = new Set(cardsSelected).size;
   const hasWon = cardsSelected.length === cardsIds.length;
-  const hasLost = (function () {
-    if (cardsSelected.length === 0) {
-      return false;
-    }
+  const hasLost = cardsSelected.length !== score;
 
-    return cardsSelected.length !== score;
-  })();
-
-  if (hasWon) {
-    alert("won!");
-  }
-
-  if (hasLost) {
-    alert("lost!");
+  if (hasWon || hasLost) {
+    endGame();
+  } else {
+    updateScore(score);
   }
 
   function handleSelection(e) {
@@ -35,18 +25,42 @@ export default function Gameboard({ imgUrls, updateScore }) {
     setCardsIds(shuffle(cardsIds));
     setCardsSelected([...cardsSelected, selectedCardId]);
   }
+  function endGame() {
+    if (hasWon) {
+      alert("won!");
+    } else if (hasLost) {
+      alert("lost!");
+    }
+
+    updateScore(0);
+    setCardsSelected([]);
+  }
+  function updateScore(score) {
+    if (score === currentScore) {
+      return;
+    }
+
+    if (score > bestScore) {
+      setBestScore(score);
+    }
+
+    setCurrentScore(score);
+  }
 
   return (
-    <div className="gameboard">
-      {cardsIds.map((id) => (
-        <Card
-          key={id}
-          cardId={id}
-          imgUrl={imgUrls[id]}
-          handleSelection={handleSelection}
-          selected={cardsSelected.includes(id)}
-        />
-      ))}
-    </div>
+    <>
+      <Score bestScore={bestScore} currentScore={currentScore} />
+      <div className="gameboard">
+        {cardsIds.map((id) => (
+          <Card
+            key={id}
+            cardId={id}
+            imgUrl={imgUrls[id]}
+            handleSelection={handleSelection}
+            selected={cardsSelected.includes(id)}
+          />
+        ))}
+      </div>
+    </>
   );
 }
